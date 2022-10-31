d4f542ee1b4dfae7574aad28f35e8759ed81257c
Tiago Boeing <contato@tiagoboeing.com>
2022-10-31 18:34:38 -0300

    Redis deployment

--- src/integration/integration.module.ts
+++ src/integration/integration.module.ts
@@ -1,8 +1,9 @@
-import { Module } from "@nestjs/common";
-import { IntegrationService } from "./integration.service";
-import { IntegrationResolver } from "./integration.resolver";
-import { HttpModule } from "@nestjs/axios";
-import { RedisPubSub } from "graphql-redis-subscriptions";
+import { Module } from '@nestjs/common';
+import { IntegrationService } from './integration.service';
+import { IntegrationResolver } from './integration.resolver';
+import { HttpModule } from '@nestjs/axios';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis, { RedisOptions } from 'ioredis';
 
 @Module({
   imports: [HttpModule],
@@ -10,10 +11,19 @@ import { RedisPubSub } from "graphql-redis-subscriptions";
     IntegrationResolver,
     IntegrationService,
     {
-      provide: "PUB_SUB",
-      useValue: new RedisPubSub()
-    }
-  ]
+      provide: 'PUB_SUB',
+      useFactory: () => {
+        const options: RedisOptions = {
+          host: 'redis-cluster.redis.svc.cluster.local',
+          port: 6379,
+        };
+
+        return new RedisPubSub({
+          publisher: new Redis(options),
+          subscriber: new Redis(options),
+        });
+      },
+    },
+  ],
 })
-export class IntegrationModule {
-}
+export class IntegrationModule {}
