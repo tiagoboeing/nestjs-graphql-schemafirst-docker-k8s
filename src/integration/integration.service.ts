ebe00aed01fdc4c1b75ef5c40245378142f8bd69
Tiago Boeing <contato@tiagoboeing.com>
2022-11-08 16:19:53 -0300

    README
    Co-authored-by: Paulo Weber <paulombweber@users.noreply.github.com>

--- src/integration/integration.service.ts
+++ src/integration/integration.service.ts
@@ -1,4 +1,3 @@
-import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PubSubEngine } from 'graphql-subscriptions';
@@ -8,7 +7,6 @@ import { CreateIntegrationInput } from './dto/create-integration.input';
 @Injectable()
 export class IntegrationService {
   private readonly logger = new Logger(IntegrationService.name);
-  private readonly apiBaseUrl: string;
   private readonly integrationInterval: number;
   private readonly integrationLimit: number;
 
@@ -17,11 +15,9 @@ export class IntegrationService {
   apiResponse = { data: [] };
 
   constructor(
-    private readonly httpService: HttpService,
     private readonly configService: ConfigService,
     @Inject('PUB_SUB') private pubSub: PubSubEngine,
   ) {
-    this.apiBaseUrl = this.configService.get('SERVICE_LOGISTICA_REVERSA');
     this.integrationInterval = this.configService.get('INTEGRATION_INTERVAL');
     this.integrationLimit = this.configService.get('INTEGRATION_LIMIT');
   }
@@ -29,8 +25,6 @@ export class IntegrationService {
   async create(
     createIntegrationInput: CreateIntegrationInput,
   ): Promise<boolean> {
-    this.logger.log(this.apiBaseUrl);
-
     await this.createSTD(createIntegrationInput.id);
 
     this.interval = setInterval(async () => {
@@ -47,19 +41,6 @@ export class IntegrationService {
       this.logger.log(
         `Creating integration for id ${createIntegrationInput.id}...`,
       );
-
-      // const apiResponse = await this.httpService
-      //   .get<any>(`${this.apiBaseUrl}/api/consultarstd`)
-      //   .pipe(
-      //     map((response) => {
-      //       return response.data;
-      //     }),
-      //     catchError((error) => {
-      //       this.logger.log(error.response.data);
-      //       throw error;
-      //     }),
-      //   )
-      //   .toPromise();
 
       this.apiResponse.data.push({
         etapa: `${this.iterations + 1}`,
