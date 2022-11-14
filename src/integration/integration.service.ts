82c32ecf614d10332ec5830afcfce64c4db8e239
Paulo Weber <pauloweber@hotmail.com.br>
2022-11-14 16:48:04 -0300

    fix: :bug: Ajuste para que cada intervalo de notificação seja independente

--- src/integration/integration.service.ts
+++ src/integration/integration.service.ts
@@ -1,71 +1,27 @@
 import { Inject, Injectable, Logger, Scope } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PubSubEngine } from 'graphql-subscriptions';
-import { setInterval } from 'timers';
 import { CreateIntegrationInput } from './dto/create-integration.input';
+import { IntegrationWorker } from './integration.worker';
 
 @Injectable({ scope: Scope.REQUEST })
 export class IntegrationService {
   private readonly logger = new Logger(IntegrationService.name);
-  private readonly integrationInterval: number;
-  private readonly integrationLimit: number;
-
-  interval = null;
-  iterations = 0;
-  apiResponse = { data: [] };
 
   constructor(
     private readonly configService: ConfigService,
     @Inject('PUB_SUB') private pubSub: PubSubEngine,
-  ) {
-    this.integrationInterval = this.configService.get('INTEGRATION_INTERVAL');
-    this.integrationLimit = this.configService.get('INTEGRATION_LIMIT');
-  }
+  ) {}
 
   async create(
     createIntegrationInput: CreateIntegrationInput,
   ): Promise<boolean> {
     await this.createSTD(createIntegrationInput.id);
-
-    this.interval = setInterval(async () => {
-      if (this.iterations >= this.integrationLimit) {
-        this.iterations = 0;
-
-        this.logger.log(
-          `Integration finished for id ${createIntegrationInput.id}`,
-        );
-
-        return clearInterval(this.interval);
-      }
-
-      this.logger.log(
-        `Creating integration for id ${createIntegrationInput.id}...`,
-      );
-
-      this.apiResponse.data.push({
-        etapa: `${this.iterations + 1}`,
-      });
-
-      if (this.apiResponse.data) {
-        const steps = this.apiResponse.data.map((step) => ({
-          step: step.etapa,
-          id: createIntegrationInput.id,
-        }));
-
-        this.logger.log(steps);
-
-        await this.pubSub.publish('integration', {
-          id: createIntegrationInput.id,
-          steps,
-        });
-      }
-
-      this.iterations++;
-      this.apiResponse.data = [];
-
-      this.logger.log(this.iterations);
-    }, this.integrationInterval);
-
+    new IntegrationWorker(
+      createIntegrationInput.id,
+      this.configService,
+      this.pubSub,
+    ).doWork();
     return true;
   }
 
