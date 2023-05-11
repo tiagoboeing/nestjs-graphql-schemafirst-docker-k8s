defde026e461774a1725ee3653426bda7e88d28d
Tiago Boeing <contato@tiagoboeing.com>
2023-05-11 11:04:48 -0300

    feat: :sparkles: Allow to start dev server using HTTPS (#4)
    Changed the default behaviour to start dev server using HTTPS.
    
    Change this anytime using the environment variable:
    
    ```
    LOCALHOST_HTTPS=true
    ```

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { AppModule } from './app.module';
+import fs from 'fs';
+import { isProduction, useSSL } from './@core/environments';
+
+const httpsOptions = {
+  cert: fs.readFileSync('./certificates/cert.pem'),
+  key: fs.readFileSync('./certificates/key.pem'),
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    httpsOptions: useSSL && !isProduction ? httpsOptions : undefined,
+    cors: {
+      origin: '*',
+    },
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  // Starts listening for shutdown hooks - Kubernetes
-  app.enableShutdownHooks();
-
-  await app.listen(process.env.SERVER_PORT || 3000);
+  await app.listen(process.env.SERVER_PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 
   app.useLogger(app.get(Logger));
