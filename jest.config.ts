29ed558acb187f5c0f0960ad188b3649ce01b265
Tiago Boeing <contato@tiagoboeing.com>
2024-01-30 14:49:13 -0300

    test: :white_check_mark: Create own file for Jest configs

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coveragePathIgnorePatterns: ['.module.ts', 'graphql.ts', 'dto.ts'],
+  coverageDirectory: '../coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 50,
+    },
+  },
+  testEnvironment: 'node',
+  testResultsProcessor: 'jest-sonar-reporter',
+  clearMocks: true,
+  detectOpenHandles: true,
+};
+
+export default config;
